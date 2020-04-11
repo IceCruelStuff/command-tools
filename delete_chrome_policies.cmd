7c057f156d0c1ae7d75b8b530ff69a3ed605d63d
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-11 14:44:05 -0700

    Create delete_chrome_policies.cmd

---
+++ delete_chrome_policies.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+rem This is useful when you want to delete policies on Chrome
+
+IF NOT EXIST %WINDIR%\System32\GroupPolicy goto next
+
+echo Deleting GroupPolicy folder...
+RD /S /Q "%WINDIR%\System32\GroupPolicy" || goto error
+echo.
+
+:next
+IF NOT EXIST %WINDIR%\System32\GroupPolicyUsers goto next2
+
+echo Deleting GroupPolicyUsers folder...
+RD /S /Q "%WINDIR%\System32\GroupPolicyUsers" || goto error
+echo.
+
+:next2
+gpupdate /force
+
+pause
+exit
+
+:error
+echo.
+echo An unexpected error has occurred. ¨Have opened the program as an administrator (right click, run as administrator)?
+echo.
+pause
+exit
